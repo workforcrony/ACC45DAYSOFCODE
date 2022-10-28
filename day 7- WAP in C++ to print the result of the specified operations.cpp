21afacce48f48848988d30a38f3a5f6dec143a30
Vivek Yadav <96163522+workforcrony@users.noreply.github.com>
2022-10-28 23:43:57 +0530

    WAP in C++ to print the result of the specified operations

---
+++ day 7- WAP in C++ to print the result of the specified operations.cpp
@@ -0,0 +1,22 @@
+/* WAP in C++ to print the result of the specified operations.
+Sample Output:
+Print the result of some specific operation :
+--------------------------------------------------
+Result of 1st expression is : 23
+Result of 2nd expression is : 5
+Result of 3rd expression is : 12
+Result of 4th expression is : 3
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Print the result of some specific operation :"<<endl;
+	cout<<"Result of 1st expression is: "<< -10+11*3 <<endl;
+	cout<<"Result of 2nd expression is: "<< -10+15*1 <<endl;
+	cout<<"Result of 3rd expression is: "<< 2+20/2 <<endl;
+	cout<<"Result of 4th expression is: "<< 7*5+10/2*5-57 <<endl;
+	return 0;
+}
+
